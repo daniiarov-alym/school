0290cfed69655f6cea3a507f908e72ec886ba44a
Aikokul Tashpulatova <tashpulatova_a@auca.kg>
2023-01-25 23:14:56 -0500

    Add accreditation page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,12 +20,12 @@ export default function Navbar() {
 							</a>
 						</li>
 						<li>
-							<a href="/" class="block py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+							<a href="/accreditation" class="block py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
 								Аккредитация
 							</a>
 						</li>
 						<li>
-							<a href="/" class="block py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+							<a href="/people" class="block py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
 								Мугалимдер
 							</a>
 						</li>
